40fa497681ff4dcc28481aa2d5096cef46c606ca
chisfy <fyzac96@gmail.com>
2023-11-12 16:07:50 +0000

    task 1 and styling done mobile first

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,27 @@
+import BlogPost from "../BlogPost";
+
 function App() {
-  return null;
-}
+
+  const blog =
+  {
+    title: "My First Post",
+    author: "Chris Meah",
+    datePosted: "20/11/2019",
+    content: `
+      A structure used in most apps and games.
+      It's a way to keep doing the same.
+      While a condition is true,
+      Or for one to twenty-two.
+      If endless, for errors we blame
+      ..........
+      Loop `,
+    imageSrc: "https://images.pexels.com/photos/1181472/pexels-photo-1181472.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    imageAlt: "A couple of coders."
+  }
+  
+  return (
+    <BlogPost blog={blog}/>
+  )
+  }
 
 export default App;
--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,5 +1,16 @@
-function BlogPost() {
-  return null;
+function BlogPost({ blog }) {
+
+  return (
+    <div className="blog-container">
+      <h1 className="title">{blog.title}</h1>
+      <h3 className="author"> By {blog.author}</h3>
+      <p className="date">{blog.datePosted}</p>
+      <hr />
+      <img src={blog.imageSrc}/>
+      <figcaption className="figc">{blog.imageAlt}</figcaption>
+      <p className="content">{blog.content}</p>
+    </div>
+  );
 }
 
 export default BlogPost;
