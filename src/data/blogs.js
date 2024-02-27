b1a88f1e71fa2109346ddd2f190a72087fdc1318
chisfy <fyzac96@gmail.com>
2024-02-27 16:47:37 +0000

    hot reloading working, dev docker container and production conatiner created and successfully working

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -3,7 +3,6 @@ export const blog = {
   author: "Marge B. Simpson",
   datePosted: "20/11/2019",
   content: `I'm always getting the kids to take one to show + tell. I just think they're neat!`,
-  imageSrc:
-    "https://i.imgur.com/hY1sb.jpg",
-  imageAlt: "This one is pretty big...",
+  imageSrc: "https://i.imgur.com/hY1sb.jpg",
+  imageAlt: "This one is pretty big...testing!",
 };
