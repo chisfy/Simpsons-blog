a52ab021c66b6a0a1c4bce1944e4280fe36a605e
chisfy <fyzac96@gmail.com>
2024-02-27 17:04:00 +0000

    dockerisation successful

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -4,5 +4,5 @@ export const blog = {
   datePosted: "20/11/2019",
   content: `I'm always getting the kids to take one to show + tell. I just think they're neat!`,
   imageSrc: "https://i.imgur.com/hY1sb.jpg",
-  imageAlt: "This one is pretty big...testing!",
+  imageAlt: "This one is pretty big...",
 };
